86535650b0ea71cd6d1dee9db3f3e08f6d273f93
Maciej Wielgosz <maciej.wielgosz@nibio.no>
2023-07-26 09:13:09 +0200

    docker oracle works locally

---
+++ run_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CONTAINER_NAME="test_e2e_instance"
+IMAGE_NAME="nibio/e2e-instance"
+
+# Check if the container exists
+if [ $(docker container ls -a -q -f name=$CONTAINER_NAME) ]; then
+    echo "Removing existing container $CONTAINER_NAME"
+    docker container rm $CONTAINER_NAME
+else
+    echo "Container $CONTAINER_NAME does not exist."
+fi
+
+# Check if the image exists
+# if [ $(docker image ls -q -f reference=$IMAGE_NAME) ]; then
+#     echo "Removing existing image $IMAGE_NAME"
+#     docker image rm $IMAGE_NAME
+# else
+#     echo "Image $IMAGE_NAME does not exist."
+# fi
+
+./build.sh
+
+echo "Running the container"
+# docker run -it --gpus all --name $CONTAINER_NAME $IMAGE_NAME > e2e-instance.log 2>&1
+
+docker run -it --gpus all --name $CONTAINER_NAME $IMAGE_NAME 
